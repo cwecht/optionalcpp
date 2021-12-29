3147dfae79aafbd9cbd7e946b26289761f41b6e0
Christopher Wecht <wechtc@gmail.com>
2021-12-29 19:29:52 +0100

    Added the first constructors

--- include/optional.hpp
+++ include/optional.hpp
@@ -2,6 +2,14 @@
 #define OPTIONALCPP_OPTIONAL_HPP
 
 struct optional_unsigned_int {
+
+  optional_unsigned_int()
+    : is_set(false) {}
+
+  optional_unsigned_int(unsigned int v)
+    : is_set(true)
+    , value(v) {};
+
   bool is_set;
   unsigned int value;
 };
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,3 +1,15 @@
 #include "catch_with_main.hpp"
 
 #include "optional.hpp"
+
+TEST_CASE("A default constructed optional is not set.") {
+  optional_unsigned_int x;
+  REQUIRE(x.is_set == false);
+}
+
+TEST_CASE("An optional constructed with a value is set and stores the value.") {
+  unsigned int anyValue = 10;
+  optional_unsigned_int x(anyValue);
+  REQUIRE(x.is_set == true);
+  REQUIRE(x.value == anyValue);
+}
