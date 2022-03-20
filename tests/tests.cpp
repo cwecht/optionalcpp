7996cafc60448bf80cb5d05c3132415913fe00ba
Christopher Wecht <wechtc@gmail.com>
2022-03-20 20:37:28 +0100

    Make optional const correct

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -2,14 +2,14 @@
 #include "optional.hpp"
 
 TEST_CASE("A default constructed optional has no value.") {
-  optional_unsigned_int x;
+  const optional_unsigned_int x;
   REQUIRE(x.has_value() == false);
 }
 
 TEST_CASE(
     "An optional constructed with a value has a value and stores the value.") {
   unsigned int anyValue = 10;
-  optional_unsigned_int x(anyValue);
+  const optional_unsigned_int x(anyValue);
   REQUIRE(x.has_value() == true);
   REQUIRE(x.value() == anyValue);
 }
